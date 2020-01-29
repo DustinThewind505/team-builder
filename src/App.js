dec2e8f33974e5c5e0c078b3d52df2b9978f1095
unknown <Dustin.Guillen14@yahoo.com>
2020-01-29 16:18:20 -0700

    first part done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Person from './components/Person';
+//import Form from './components/Form';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [team, setTeam] = useState([
+    {
+      id: 1,
+      title: "Awwww here it goes",
+      body: "Kenan and Kel",
+      name: "Kel Mitchell",
+      email: "Kel@OrangeSoda.com",
+      role: "Best Friend"
+    },
+
+    {
+      id: 2,
+      title: "Awwww here it goes",
+      body: "Kenan and Kel",
+      name: "Kenan Thompson",
+      email: "Kenan@SNL.com",
+      role: "Mastermind"
+    }
+
+  ])
   return (
     <div className="App">
+      
       <header className="App-header">
+      <Person person={team}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just Do It!
         </p>
         <a
           className="App-link"
@@ -16,9 +43,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets Do This!
         </a>
       </header>
+      
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
+function Person(props) {
+   // console.log(props.person[0])
 
+    return (
+        <div>
+            {props.person.map(perp =>{ 
+                return(
+            <div key={perp.id}>
+                <h2>{perp.title}</h2>
+                <h3>{perp.body}</h3>
+                <p>{perp.name}</p>
+                <p>{perp.email}</p>
+                <p>{perp.role}</p>
+
+            </div>
+            )
+            })}
+        </div>
+    )
+}
 
 
 export default Person;
